37f30307466427f20cff97c2aef472c48cbc39f0
Mohamed <Mohamed@Stalker.com>
2017-06-04 19:02:41 +0100

    Rename example page

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+// Redirect HTTPS to HTTP (Weathermap Free plan doesn't work with HTTPS')
 app.use(function (req, res, next) {
     if (req.headers['x-forwarded-proto'] === 'http') {
         next();
