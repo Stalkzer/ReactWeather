d59b014486f9865b8e5a915cf91687c70ad34675
Mohamed <Mohamed@Stalker.com>
2017-06-04 21:19:23 +0100

    Adds styles

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
-var React = require("react");
+var React = require("react"),
+    {Link} = require("react-router");
 
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>Just an example page!</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are some example location to try out :D</p>
+            <ol>
+                <li>
+                    <Link to="/?location=blida"> Blida </Link>
+                </li>
+                <li>
+                    <Link to="/?location=paris"> Paris </Link>
+                </li>
+            </ol>
         </div>
     )
 };
