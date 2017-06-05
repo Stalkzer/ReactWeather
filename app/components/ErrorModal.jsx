7f482480fd5843ea8342b3ff13e8d0b91a6081d3
Mohamed <Mohamed@Stalker.com>
2017-06-05 15:54:11 +0100

    Adds modal message

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -6,15 +6,20 @@ var ErrorModal = React.createClass({
             title: "Error"
         };
     },
+    propTypes: {
+        title: React.PropTypes.string,
+        message: React.PropTypes.string.isRequired
+    },
     componentDidMount: function () {
         var modal = new Foundation.Reveal($('#error-modal'));
         modal.open();
     },
     render: function () {
+        var {title,message} = this.props;
         return (
             <div className="reveal tiny text-center" id="error-modal" data-reveal="">
-                <h4>Error</h4>
-                <p>City not found</p>
+                <h4>{title}</h4>
+                <p>{message}</p>
                 <p>
                     <button className="button hollow" data-close="">
                         Okay
